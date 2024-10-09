3539d4d1dd79a5b408a92de3e1fb9d3466a08ce2
MBURKU <rku@aarhus.dk>
2024-10-09 14:37:13 +0200

    Changed manuel list to handle different types of lists based on the process_status from table database.

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 from itk_dev_shared_components.smtp import smtp_util
 from robot_framework import config
-from robot_framework.subprocesses.manuel_list_handler import ManuelListHandler
+from robot_framework.subprocesses.list_handler import ListHandler
 
 
 def process(orchestrator_connection: OrchestratorConnection) -> None:
@@ -12,30 +12,33 @@ def process(orchestrator_connection: OrchestratorConnection) -> None:
     orchestrator_connection.log_trace("Running process.")
 
     rpa_db_connection = orchestrator_connection.get_constant("DbConnectionString").value
+    oc_args_json = json.loads(orchestrator_connection.process_arguments)
 
-    man_list = ManuelListHandler(rpa_db_connection)
+    list_types = ['manuel', 'successful']
 
-    try:
-        orchestrator_connection.log_trace("Sending e-mail.")
-        oc_args_json = json.loads(orchestrator_connection.process_arguments)
+    for list_type in list_types:
+        list_report = ListHandler(rpa_db_connection, list_type)
 
-        receiver = [oc_args_json['emailReceiver1'], oc_args_json['emailReceiver2']]
-        sender = oc_args_json['emailSender']
-        now = datetime.now().strftime("%d-%m-%y")
-        subject = f"Journalisering - Solteq Tand - Manuel liste d. {now}"
-        body = man_list.generate_list()
+        try:
+            orchestrator_connection.log_trace("Sending e-mail.")
 
-        smtp_util.send_email(
-            receiver=receiver,
-            sender=sender,
-            subject=subject,
-            body=body,
-            html_body=True,
-            smtp_server=config.SMTP_SERVER,
-            smtp_port=config.SMTP_PORT
-        )
+            html_content, title = list_report.generate_list()
+            receiver = [oc_args_json['emailReceiver1'], oc_args_json['emailReceiver2']]
+            sender = oc_args_json['emailSender']
+            now = datetime.now().strftime("%d-%m-%y")
+            subject = f"Journalisering - Solteq Tand - {title} d. {now}"
 
-        orchestrator_connection.log_trace(f"E-mail was sent to: {receiver}")
+            smtp_util.send_email(
+                receiver=receiver,
+                sender=sender,
+                subject=subject,
+                body=html_content,
+                html_body=True,
+                smtp_server=config.SMTP_SERVER,
+                smtp_port=config.SMTP_PORT
+            )
 
-    except (Exception, RuntimeError) as e:
-        raise e
+            orchestrator_connection.log_trace(f"E-mail was sent to: {receiver}")
+
+        except (Exception, RuntimeError) as e:
+            raise e
