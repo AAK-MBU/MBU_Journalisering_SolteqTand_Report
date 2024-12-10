6e9d5259f477225b98d0706ad078a0fab8fbd71d
MBURKU <rku@aarhus.dk>
2024-12-10 12:08:09 +0100

    Merged the report into one list.

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -14,31 +14,28 @@ def process(orchestrator_connection: OrchestratorConnection) -> None:
     rpa_db_connection = orchestrator_connection.get_constant("DbConnectionString").value
     oc_args_json = json.loads(orchestrator_connection.process_arguments)
 
-    list_types = ['manuel', 'successful']
+    try:
+        orchestrator_connection.log_trace("Generating the report.")
+        list_report = ListHandler(rpa_db_connection)        
 
-    for list_type in list_types:
-        list_report = ListHandler(rpa_db_connection, list_type)
+        html_content = list_report.generate_list()
+        orchestrator_connection.log_trace("Sending e-mail.")
+        receiver = [oc_args_json['emailReceiver1'], oc_args_json['emailReceiver2']]
+        sender = oc_args_json['emailSender']
+        now = datetime.now().strftime("%d-%m-%y")
+        subject = f"Journalisering - Solteq Tand - Rapport d. {now}"
 
-        try:
-            orchestrator_connection.log_trace("Sending e-mail.")
+        smtp_util.send_email(
+            receiver=receiver,
+            sender=sender,
+            subject=subject,
+            body=html_content,
+            html_body=True,
+            smtp_server=config.SMTP_SERVER,
+            smtp_port=config.SMTP_PORT
+        )
 
-            html_content, title = list_report.generate_list()
-            receiver = [oc_args_json['emailReceiver1'], oc_args_json['emailReceiver2']]
-            sender = oc_args_json['emailSender']
-            now = datetime.now().strftime("%d-%m-%y")
-            subject = f"Journalisering - Solteq Tand - {title} d. {now}"
+        orchestrator_connection.log_trace(f"E-mail was sent to: {receiver}")
 
-            smtp_util.send_email(
-                receiver=receiver,
-                sender=sender,
-                subject=subject,
-                body=html_content,
-                html_body=True,
-                smtp_server=config.SMTP_SERVER,
-                smtp_port=config.SMTP_PORT
-            )
-
-            orchestrator_connection.log_trace(f"E-mail was sent to: {receiver}")
-
-        except (Exception, RuntimeError) as e:
-            raise e
+    except (Exception, RuntimeError) as e:
+        raise e
